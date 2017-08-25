ab97b63325bfdf9d92ed5338c6c1f8c0d1526114
surayez <surayez@yahoo.com>
2017-08-25 18:37:23 +1000

    Creating webpage

--- js/script.js
+++ js/script.js
@@ -4,63 +4,70 @@ var currentEventNum = 0;
 var eventItems;
 var openList = [];
 var closedList = [];
+var dataReceived;
 
 $(document).ready(function () {
-    $.getJSON( "temp.json", function( data ) {
-        eventItems = [];
-        $.each( data, function( key, val ) {
-            eventItems.push(val);
-            //Can be accessed by eventItems[0][i] in i loop
+    $('#dataLoader').click(function(){
+        var ref = firebase.database().ref("/");
+        ref.once("value").then(function (snapshot) {
+            dataReceived = snapshot.val();
+            dataReceived = dataReceived.data;
+            if(dataReceived!=null) {
+                window.alert("Data is loaded");
+            }
         });
-
     });
 
-    $('.playbtn').click(function () {
-        for(i=currentEventNum;i<=eventItems[0].length-1;i++) {
-            var eventli = document.createElement("LI");
-            var newMainItem = document.createTextNode(eventItems[0][i].type + ", x= " + eventItems[0][i].x + ", y= " +eventItems[0][i].y + ", g= " +eventItems[0][i].g + ", h= " +eventItems[0][i].h);
-            currentEventNum += 1;
+    $('.buildbtn').click(function () {
+        if(dataReceived!=null) {
+            for (i = currentEventNum; i <= dataReceived.length - 1; i++) {
+                var eventli = document.createElement("LI");
+                var newMainItem = document.createTextNode(dataReceived[i].type + ", x= " + dataReceived[i].x + ", y= " + dataReceived[i].y + ", g= " + dataReceived[i].g + ", h= " + dataReceived[i].h);
+                currentEventNum += 1;
 
-            if(eventItems[0][i].type == 'expanding'){
-                var openListli = document.createElement("LI");
-                openList.push(eventItems[0][i].x);
-                var openListItem = document.createTextNode("["+openList+"]");
-                openListli.appendChild(openListItem)
+                if (dataReceived[i].type == 'expanding') {
+                    var openListli = document.createElement("LI");
+                    openList.push(dataReceived[i].x);
+                    var openListItem = document.createTextNode("[" + openList + "]");
+                    openListli.appendChild(openListItem)
+                }
+
+                if (dataReceived[i].type == 'closing') {
+                    openList.pop(dataReceived[i].x);
+                    var closedListli = document.createElement("LI");
+                    closedList.push(dataReceived[i].x);
+                    var closedListItem = document.createTextNode("[" + closedList + "]");
+                    closedListli.appendChild(closedListItem)
+                }
+
+                // Append the text to <li>
+                eventli.appendChild(newMainItem);
+                $('#openListConsole').append(openListli);
+                $('#closedListConsole').append(closedListli);
+                $('#eventList').append(eventli);
             }
 
-            if(eventItems[0][i].type == 'closing'){
-                openList.pop(eventItems[0][i].x);
-                var closedListli = document.createElement("LI");
-                closedList.push(eventItems[0][i].x);
-                var closedListItem = document.createTextNode("["+closedList+"]");
-                closedListli.appendChild(closedListItem)
-            }
-
-            // Append the text to <li>
-            eventli.appendChild(newMainItem);
-
-        $('#openListConsole').append(openListli);
-        $('#closedListConsole').append(closedListli);
-        $('#eventList').append(eventli);
+            var mydiv = $(".eventLog");
+            mydiv.scrollTop(mydiv.prop("scrollHeight"));
+        }else{
+            window.alert("No data loaded. Please select file and load data.")
         }
-
-	var mydiv = $(".eventLog");
-	mydiv.scrollTop(mydiv.prop("scrollHeight"));
     });
 
     $('.stepbtn').click(function () {
         var eventli = document.createElement("LI");                             // Create a <li> node
-        var newMainItem = document.createTextNode(eventItems[0][currentEventNum].type + ", x= " + eventItems[0][currentEventNum].x + ", y= " +eventItems[0][currentEventNum].y + ", g= " +eventItems[0][currentEventNum].g + ", h= " +eventItems[0][currentEventNum].h);          // Create a text node
-        currentEventNum += 1;
-            if (eventItems[0][currentEventNum].type == 'expanding') {
+        if(currentEventNum<=dataReceived.length) {
+            var newMainItem = document.createTextNode(dataReceived[currentEventNum].type + ", x= " + dataReceived[currentEventNum].x + ", y= " + dataReceived[currentEventNum].y + ", g= " + dataReceived[currentEventNum].g + ", h= " + dataReceived[currentEventNum].h);          // Create a text node
+            currentEventNum += 1;
+            if (dataReceived[currentEventNum].type == 'expanding') {
                 var openListli = document.createElement("LI");
-                openList.push(eventItems[0][currentEventNum].x);
+                openList.push(dataReceived[currentEventNum].x);
                 var openListItem = document.createTextNode("[" + openList + "]");
                 openListli.appendChild(openListItem)
             }
 
-            if (eventItems[0][currentEventNum].type == 'closing') {
-                openList.pop(eventItems[0][currentEventNum].x);
+            if (dataReceived[currentEventNum].type == 'closing') {
+                openList.pop(dataReceived[currentEventNum].x);
                 var closedListli = document.createElement("LI");
                 closedList.push(eventItems[0][i].x);
                 var closedListItem = document.createTextNode("[" + closedList + "]");
@@ -75,7 +82,24 @@ $(document).ready(function () {
 
             var mydiv = $(".eventLog");
             mydiv.scrollTop(mydiv.prop("scrollHeight"));
+
+        }
+    });
+
+    $("#inputFile").click(function(){
+        $.getJSON( "temp.json", function( data ) {
+            eventItems = [];
+            $.each(data, function (key, val) {
+                eventItems.push(val);
+                //Can be accessed by eventItems[0][i] in i loop
+            });
+        firebase.database().ref('/').set({
+            data : eventItems[0]
+            });
         });
+    })
 
 });
+
+
 
