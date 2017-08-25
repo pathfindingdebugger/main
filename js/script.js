de95e10b8139b598eaca7e1fcf294ae71aeae418
surayez <surayez@yahoo.com>
2017-08-25 10:47:55 +1000

    Creating webpage

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 
 var sampleText = "Sample text here";
 var sampleArray = ['ab','3e', '232'];
+var currentEventNum = 0;
 var eventItems;
 
 $(document).ready(function () {
@@ -16,13 +17,15 @@ $(document).ready(function () {
     });
 
     $('.playbtn').click(function () {
-        for(i=0;i<=eventItems[0].length-1;i++) {
+        for(i=currentEventNum;i<=eventItems[0].length-1;i++) {
             var newList = document.createElement("LI");                             // Create a <li> node
             // Create a text node
             var newItem = document.createTextNode(eventItems[0][i].type + ", x= " + eventItems[0][i].x + ", y= " +eventItems[0][i].y + ", g= " +eventItems[0][i].g + ", h= " +eventItems[0][i].h);
+            currentEventNum += 1;
             newList.appendChild(newItem);                                           // Append the text to <li>
+        $('#openListConsole').append(newList);
+        $('#eventList').append(newList);
 
- 	$('#eventList').append(newList);
 
         }
 
@@ -33,7 +36,8 @@ $(document).ready(function () {
     $('.stepbtn').click(function () {
         var newList = document.createElement("LI");                             // Create a <li> node
 
-        var newItem = document.createTextNode(obj.expanding[0].type);          // Create a text node
+        var newItem = document.createTextNode(eventItems[0][currentEventNum].type + ", x= " + eventItems[0][currentEventNum].x + ", y= " +eventItems[0][currentEventNum].y + ", g= " +eventItems[0][currentEventNum].g + ", h= " +eventItems[0][currentEventNum].h);          // Create a text node
+        currentEventNum += 1;
         newList.appendChild(newItem);                                           // Append the text to <li>
 
         $('#eventList').append(newList);
